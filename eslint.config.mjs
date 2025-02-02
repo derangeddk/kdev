e60a3ac4b8f1d3891854c9f05969fa87edf5e32d
Asbjørn Dyhrberg Thegler <asbjoern@thegler.dk>
2025-02-02 15:50:07 +0100

    Add linting

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    pluginJs.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+              ...globals.node,
+              // add zx globals
+              '$': false,
+              echo: false,
+              chalk: false,
+              YAML: false,
+              fs: false,
+              question: false,
+            }
+        }
+    },
+];
