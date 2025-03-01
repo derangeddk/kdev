5c172394b30d310965eb2d54eb8f165247bf8856
Asbjørn Dyhrberg Thegler <asbjoern@thegler.dk>
2025-03-01 22:32:07 +0100

    Improve error handling

--- bin/kdev.mjs
+++ bin/kdev.mjs
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+import { chalk, echo } from 'zx';
 
 process.env.FORCE_COLOR='1'
+
+process.on('warning', (warning) => {
+    echo(chalk.orange('A warning was emitted'));
+    echo(warning);
+});
 
 const args = process.argv.slice(2);
 const command = args.shift();
@@ -10,4 +16,11 @@ if (!command) {
     process.exit(1);
 }
 
-await import(`./${command}.mjs`);
+
+try {
+    await import(`./${command}.mjs`);
+} catch (error) {
+    echo(chalk.red('An error happened'));
+    echo(error);
+    process.exit(1);
+}
