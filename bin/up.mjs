454625d7486e95e5b1401597c997b09042dc7e6d
Asbjørn Dyhrberg Thegler <asbjoern@dyhrbergthegler.dk>
2025-04-25 09:41:20 +0200

    Fix typo

--- bin/up.mjs
+++ bin/up.mjs
@@ -37,7 +37,7 @@ const installChart = async ({ name, remoteChart, remoteRepo = "", version, value
     echo(`helm upgrade --install --values "${temporaryFileName}" ${name} ${remoteChart || name} ${remoteRepo && `--repo ${remoteRepo}`} --version ${version}`)
     await $({ quiet: true })`helm upgrade --install --values "${temporaryFileName}" ${name} ${remoteChart || name} ${remoteRepo && `--repo ${remoteRepo}`} --version ${version}`;
   } catch (error) {
-    echo(chalk.redBright(`Could not install chart { name: ${name}`));
+    echo(chalk.redBright(`Could not install chart { name: ${name} }`));
     return echo(error);
   } finally {
     fs.rmSync(temporaryFileName);
@@ -110,7 +110,6 @@ services.push(calico());
 
 // Install default services
 services.push(installService({ name: 'nginx-ingress-controller', dir: `${import.meta.dirname}/../services/nginx-ingress-controller` }));
-services.push(installService({ name: 'cert-manager', dir: `${import.meta.dirname}/../services/cert-manager` }));
 
 // Install custom services
 if (config.spec.services) {
