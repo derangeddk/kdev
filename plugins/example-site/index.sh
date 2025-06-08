ac68cb6efb35779bcf509a7d5d3e08ff8f0ae81d
Asbjørn Dyhrberg Thegler <asbjoern@thegler.dk>
2025-06-08 17:22:34 +0200

    Add example site plugin

---
+++ plugins/example-site/index.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1;
+
+# this will be called with:
+# - 'install' to install the plugin
+COMMAND=$1;
+
+if [ "$COMMAND" == "install" ]; then
+  skaffold run 2> /dev/null;
+fi
